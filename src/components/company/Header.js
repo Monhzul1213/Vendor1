38993e5eace48faed5dd97a9059cbde7438a976d
Monhzul1213 <monhzul1213@gmail.com>
2022-10-28 11:45:52 +0800

    10-28

--- src/components/company/Header.js
+++ src/components/company/Header.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import '../../css/menu.css';
 import {User} from '../all/User'
-import logo1 from '../../assets/logo1.png'
+import logo_new from '../../assets/logo_new.png'
 import { useTranslation } from 'react-i18next';
 import { useSelector, useDispatch } from 'react-redux';
 
@@ -10,7 +10,7 @@ export function Header(){
 
   return (
     <div className='header_back'>
-      <img src={logo1} className="menu_logo_back"/>
+      <img src={logo_new} className="menu_logo_back"/>
       <p className='menu_logo_title'>{t('vendor_list')}</p>
         <User/>
     </div>
